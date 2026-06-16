7c332f1e9a4f5662d65f72be53e05f0e346b9baa
Rand Salem <rsalem6@smail.ucas.edu.ps>
2026-06-16 19:52:20 +0300

    Update startBDA.cmd

--- scripts/startBDA.cmd
+++ scripts/startBDA.cmd
@@ -1,9 +1,8 @@
 @echo off
 setlocal
 
-set "WORKINGDIRECTORY="C:\WPy64-3.13.12.0\notebooks\Dev\bda"
-::set "VENV=C:\WPy64-3.13.12.0\notebooks\Envs\bigdata"
-set "VENV=C:\WPy64-3.13.12.0\Envs\bigdata"
+set "WORKINGDIRECTORY=C:\WPy64-313130\notebooks\Dev\bda"
+set "VENV=C:\WPy64-313130\notebooks\Envs\bigdata"
 
 cd /d "%WORKINGDIRECTORY%"
 call "%VENV%\Scripts\activate.bat"
